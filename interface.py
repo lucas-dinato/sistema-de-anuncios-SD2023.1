b5ba88a46cdfd8406af8e430db6651cc7e8b9bf7
Lucas Dinato <luca.nato@hotmail.com>
2023-06-28 19:38:48 -0300

    corrigido erros de versão

--- interface.py
+++ interface.py
@@ -4,11 +4,7 @@ from __future__ import annotations
 # $ pip install --user typing_extensions
 import sys
 
-IS_NEW_PYTHON: bool = sys.version_info >= (3, 8)
-if IS_NEW_PYTHON:
-    from typing import TypeAlias
-else:
-    from typing_extensions import TypeAlias
+from typing_extensions import TypeAlias
 
 from typing import Callable, TYPE_CHECKING
 from dataclasses import dataclass
@@ -21,25 +17,15 @@ UserId: TypeAlias = str
 
 Topic: TypeAlias = str
 
-# Isso é para ser tipo uma struct
-# Frozen diz que os campos são read-only
-if IS_NEW_PYTHON:
-    @dataclass(frozen=True, kw_only=True, slots=True)
-    class Content:
-        author: UserId
-        topic: Topic
-        data: str
-elif not TYPE_CHECKING:
-    @dataclass(frozen=True)
-    class Content:
-        author: UserId
-        topic: Topic
-        data: str
 
-if IS_NEW_PYTHON:
-    FnNotify: TypeAlias = Callable[[list[Content]], None]
-elif not TYPE_CHECKING:
-    FnNotify: TypeAlias = Callable
+@dataclass(frozen=True)
+class Content:
+    author: UserId
+    topic: Topic
+    data: str
+
+
+FnNotify: TypeAlias = Callable
 
 
 class BrokerService(rpyc.Service):  # type: ignore
