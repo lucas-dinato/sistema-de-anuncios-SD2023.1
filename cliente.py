1cd5189861f236ce6d5c5053b95fa1146ac730d5
Lucas Dinato <luca.nato@hotmail.com>
2023-06-29 00:00:40 -0300

    28/06

--- cliente.py
+++ cliente.py
@@ -1,25 +1,49 @@
 import rpyc
 
+global_id = 0
+
+
+def exposed_get_id():
+    return global_id
+
+
+def callback(contents):
+    print("\nConteúdos:")
+    for content in contents:
+        print(f"{content}\n")
+    return
+
+
+def login():
+    id = input("Digite seu identificador: ")
+    retorno = system.root.login(id, callback)
+    if retorno:
+        global_id = id
+        print("Login realizado com sucesso.")
+    else:
+        print("Login inválido.")
+        login()
+
+
 system = rpyc.connect('localhost', 10000)
+login()
+print("\n")
 
 while True:
-    op = input("Digite uma funcionalidade (login, list, publish, subscribe, unsubscribe ou 'fim' para terminar): ")
-    if op == 'fim':
+    func = input("Digite uma funcionalidade (list, publish, subscribe, unsubscribe ou 'fim' para terminar): ")
+    if func == 'fim':
         system.close()
         break
 
-    if op == 'login':
-        div = system.root.login()
-        print(div)
-    elif op == 'list':
-        div = system.root.list_topics()
-        print(div)
-    elif op == 'publish':
-        div = system.root.publish()
-        print(div)
-    elif op == 'subscribe':
-        div = system.root.subscribe_to()
-        print(div)
-    elif op == 'unsubscribe':
-        div = system.root.unsubscribe_to()
-        print(div)
+    if func == 'list':
+        topics = system.root.list_topics()
+        print(topics)
+    elif func == 'publish':
+        anuncio = system.root.publish()
+        print(anuncio)
+    elif func == 'subscribe':
+        topic = system.root.subscribe_to()
+        print(topic)
+    elif func == 'unsubscribe':
+        topic = system.root.unsubscribe_to()
+        print(topic)
--- interface.py
+++ interface.py
@@ -4,7 +4,11 @@ from __future__ import annotations
 # $ pip install --user typing_extensions
 import sys
 
-from typing_extensions import TypeAlias
+IS_NEW_PYTHON: bool = sys.version_info > (3, 9)
+if IS_NEW_PYTHON:
+    from typing import TypeAlias
+else:
+    from typing_extensions import TypeAlias
 
 from typing import Callable, TYPE_CHECKING
 from dataclasses import dataclass
@@ -17,16 +21,48 @@ UserId: TypeAlias = str
 
 Topic: TypeAlias = str
 
+# Isso é para ser tipo uma struct
+# Frozen diz que os campos são read-only
+if IS_NEW_PYTHON:
+    @dataclass(frozen=True, kw_only=True, slots=True)
+    class Content:
+        author: UserId
+        topic: Topic
+        data: str
+elif not TYPE_CHECKING:
+    @dataclass(frozen=True)
+    class Content:
+        author: UserId
+        topic: Topic
+        data: str
 
-@dataclass(frozen=True)
-class Content:
-    author: UserId
-    topic: Topic
-    data: str
+if IS_NEW_PYTHON:
+    FnNotify: TypeAlias = Callable[[list[Content]], None]
+elif not TYPE_CHECKING:
+    FnNotify: TypeAlias = Callable
 
 
-FnNotify: TypeAlias = Callable
+class Usuario:
+    def __init__(self, id, inscricoes, status, anunciosRecebidos):
+        self.id = id
+        self.inscricoes = inscricoes
+        self.status = status
+        self.anunciosRecebidos = anunciosRecebidos
 
+
+usuarios = []
+
+usuariomock = Usuario("123", ["Cavalo"], False, ["Cavalo: Cavalo 2.0 lançado"])
+usuarios.append(usuariomock)
+
+# anuncios = {"123": [{"topic": "cavalo", "autor": "admin", "mensagem": "oi oi oi oi"}]}
+anuncios = {}
+# inscritos = {"Cavalo": ["123"]}
+inscritos = {}
+connected_users = {}
+
+
+# connected_users = {}
 
 class BrokerService(rpyc.Service):  # type: ignore
 
@@ -37,12 +73,41 @@ class BrokerService(rpyc.Service):  # type: ignore
     # Handshake
 
     def exposed_login(self, id: UserId, callback: FnNotify) -> bool:
-        assert False, "TO BE IMPLEMENTED"
+        if len(id) == 0:
+            return False
+        if id in connected_users.values():
+            return False
+        else:
+            for usuario in usuarios:
+                if usuario.id == id:
+                    callback(usuario.anunciosRecebidos)
+        connected_users[self.connection] = id
+        usuario = Usuario(id, [], True, [])
+        usuarios.append(usuario)
+        return True
+        # for usuario in usuarios:
+        #     if usuario.id == id:
+        #         if usuario.status:
+        #             return False
+        #         else:
+        #             callback(usuario.anunciosRecebidos)
+        #             usuario.status = True
+        #             return True
+
+        # usuario = Usuario(id, [], True)
+        # usuarios.append(usuario)
+
+        # return True
 
     # Query operations
 
     def exposed_list_topics(self) -> list[Topic]:
-        assert False, "TO BE IMPLEMENTED"
+        topics_titles = []
+
+        for content in anuncios:
+            topics_titles.append(content)
+
+        return topics_titles
 
     # Publisher operations
 
@@ -65,6 +130,16 @@ class BrokerService(rpyc.Service):  # type: ignore
         Função responde se `id` não está inscrito no `topic`
         """
         assert False, "TO BE IMPLEMENTED"
+        """
+
+    def on_connect(self, conx):
+        print("Conexao estabelecida.")
+        self.connection = conx
+
+    def on_disconnect(self, conx):
+        # Implemente a lógica que deseja executar quando um cliente se desconectar
+        print("Conexao encerrada.")
+        del connected_users[conx]
 
 
 brokerService = ThreadedServer(BrokerService, port=10000)
